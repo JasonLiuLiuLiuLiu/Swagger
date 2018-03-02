551ed1e82ae9ae85929c160ac17493366756f540
Liu ZhenYu <liuzhenyulive@live.com>
2018-03-02 11:35:43 +0800

    Swager Asp.Net Core Demo

--- Swagger/Startup.cs
+++ Swagger/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Swagger
 {
@@ -22,6 +25,14 @@ namespace Swagger
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",new Info{Title = "Hello",Version = "v1"});
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "Swagger.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+            services.AddMvcCore().AddApiExplorer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +47,16 @@ namespace Swagger
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger(c =>
+            {
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.ShowExtensions();
+                c.ValidatorUrl(null);
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "test V1");
+            });
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
